1fbebbc2cad5a398b13cc33a08b9daa4e9615b43
AyuyuAbelief <ayuyu0988@163.com>
2021-04-20 20:03:57 +0800

    update mds

--- src/index/index.js
+++ src/index/index.js
@@ -2,11 +2,16 @@ import Vue from 'vue';
 // 引入elementui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+// 引入MdsUi
+import MdsUi from '@bic-fe/mds-ui'; // 引入组件
+import '@bic-fe/mds-ui/dist/mds-ui.min.css'; // 引入组件的样式 
+
 import router from './router';
 import App from './App';
 // 使用elementUI
 Vue.use(ElementUI);
-
+// 使用MdsUi
+Vue.use(MdsUi)
 new Vue({
     el: '#app',
     router,
